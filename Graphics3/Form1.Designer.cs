0277d1294e8e4d08d9473cd3478ad5c154615269
chamomi <olgadovbnya97@gmail.com>
2018-05-20 22:05:56 +0200

    Add project files.

---
+++ Graphics3/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace Graphics3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.modeToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.dDAToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.midpointCircleAdditionsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.xaolinWuToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.thickLineToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.modeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.Thick = new System.Windows.Forms.ToolStripTextBox();
+            this.lineToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.circleToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cleanToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 27);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(613, 348);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.Draw);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.modeToolStripMenuItem1,
+            this.modeToolStripMenuItem,
+            this.cleanToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(652, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // modeToolStripMenuItem1
+            // 
+            this.modeToolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.dDAToolStripMenuItem,
+            this.midpointCircleAdditionsToolStripMenuItem,
+            this.xaolinWuToolStripMenuItem,
+            this.thickLineToolStripMenuItem});
+            this.modeToolStripMenuItem1.Name = "modeToolStripMenuItem1";
+            this.modeToolStripMenuItem1.Size = new System.Drawing.Size(50, 20);
+            this.modeToolStripMenuItem1.Text = "Mode";
+            // 
+            // dDAToolStripMenuItem
+            // 
+            this.dDAToolStripMenuItem.Name = "dDAToolStripMenuItem";
+            this.dDAToolStripMenuItem.Size = new System.Drawing.Size(214, 22);
+            this.dDAToolStripMenuItem.Text = "DDA";
+            this.dDAToolStripMenuItem.Click += new System.EventHandler(this.dDAToolStripMenuItem_Click);
+            // 
+            // midpointCircleAdditionsToolStripMenuItem
+            // 
+            this.midpointCircleAdditionsToolStripMenuItem.Name = "midpointCircleAdditionsToolStripMenuItem";
+            this.midpointCircleAdditionsToolStripMenuItem.Size = new System.Drawing.Size(214, 22);
+            this.midpointCircleAdditionsToolStripMenuItem.Text = "Midpoint circle (additions)";
+            this.midpointCircleAdditionsToolStripMenuItem.Click += new System.EventHandler(this.midpointCircleAdditionsToolStripMenuItem_Click);
+            // 
+            // xaolinWuToolStripMenuItem
+            // 
+            this.xaolinWuToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.lineToolStripMenuItem,
+            this.circleToolStripMenuItem});
+            this.xaolinWuToolStripMenuItem.Name = "xaolinWuToolStripMenuItem";
+            this.xaolinWuToolStripMenuItem.Size = new System.Drawing.Size(214, 22);
+            this.xaolinWuToolStripMenuItem.Text = "Xaolin Wu";
+            // 
+            // thickLineToolStripMenuItem
+            // 
+            this.thickLineToolStripMenuItem.Name = "thickLineToolStripMenuItem";
+            this.thickLineToolStripMenuItem.Size = new System.Drawing.Size(214, 22);
+            this.thickLineToolStripMenuItem.Text = "Pixel copying";
+            this.thickLineToolStripMenuItem.Click += new System.EventHandler(this.thickLineToolStripMenuItem_Click);
+            // 
+            // modeToolStripMenuItem
+            // 
+            this.modeToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.Thick});
+            this.modeToolStripMenuItem.Name = "modeToolStripMenuItem";
+            this.modeToolStripMenuItem.Size = new System.Drawing.Size(71, 20);
+            this.modeToolStripMenuItem.Text = "Thickness";
+            // 
+            // Thick
+            // 
+            this.Thick.Name = "Thick";
+            this.Thick.Size = new System.Drawing.Size(100, 23);
+            this.Thick.Text = "1";
+            // 
+            // lineToolStripMenuItem
+            // 
+            this.lineToolStripMenuItem.Name = "lineToolStripMenuItem";
+            this.lineToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.lineToolStripMenuItem.Text = "Line";
+            this.lineToolStripMenuItem.Click += new System.EventHandler(this.lineToolStripMenuItem_Click);
+            // 
+            // circleToolStripMenuItem
+            // 
+            this.circleToolStripMenuItem.Name = "circleToolStripMenuItem";
+            this.circleToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.circleToolStripMenuItem.Text = "Circle";
+            this.circleToolStripMenuItem.Click += new System.EventHandler(this.circleToolStripMenuItem_Click);
+            // 
+            // cleanToolStripMenuItem
+            // 
+            this.cleanToolStripMenuItem.Name = "cleanToolStripMenuItem";
+            this.cleanToolStripMenuItem.Size = new System.Drawing.Size(49, 20);
+            this.cleanToolStripMenuItem.Text = "Clean";
+            this.cleanToolStripMenuItem.Click += new System.EventHandler(this.cleanToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(652, 387);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Drawing";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem modeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem modeToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem dDAToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem midpointCircleAdditionsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem xaolinWuToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem thickLineToolStripMenuItem;
+        private System.Windows.Forms.ToolStripTextBox Thick;
+        private System.Windows.Forms.ToolStripMenuItem lineToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem circleToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem cleanToolStripMenuItem;
+    }
+}
+
---
+++ Graphics3/Form1.cs
@@ -0,0 +1,386 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Graphics3
+{
+    public partial class Form1 : Form
+    {
+        private int x1, y1, x2, y2;
+        private Bitmap img;
+        private bool dda, midpoint, xline, xcircle, copy;
+
+        public Form1()
+        {
+            InitializeComponent();
+            img = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            pictureBox1.Image = img;
+            x1 = x2 = y1 = y2 = -1;
+            dda = true;
+            midpoint = xline = xcircle = copy = false;
+        }
+
+        private void Draw(object sender, EventArgs e)
+        {
+            int x = ((MouseEventArgs)e).X;
+            int y = ((MouseEventArgs)e).Y;
+
+            if (x1 == -1 || y1 == -1)
+            {
+                x1 = x; y1 = y;
+            }
+            else
+            {
+                x2 = x; y2 = y;
+                if (dda)
+                    DDA(x1, pictureBox1.Height - y1, x2, pictureBox1.Height - y2);
+                else if (midpoint)
+                    MidpointCircle(x1, pictureBox1.Height - y1, x2, pictureBox1.Height - y2);
+                else if (xline)
+                    XLine(x1, pictureBox1.Height - y1, x2, pictureBox1.Height - y2);
+                else if (xcircle)
+                    XCircle(x1, pictureBox1.Height - y1, x2, pictureBox1.Height - y2);
+                else if (copy)
+                    PixelCopying(x1, pictureBox1.Height - y1, x2, pictureBox1.Height - y2);
+
+                x1 = x2 = y1 = y2 = -1;
+            }
+            pictureBox1.Image = img;
+        }
+
+        private void DDA(int x1, int y1, int x2, int y2)
+        {
+            float step;
+            float diffy = Math.Abs(y2 - y1);
+            float diffx = Math.Abs(x2 - x1);
+
+            if (diffx >= diffy)
+                step = diffx;
+            else
+                step = diffy;
+
+            diffx = diffx / step;
+            diffy = diffy / step;
+
+            float x = x1;
+            float y = y1;
+            int i = 1;
+
+            while (i <= step)
+            {
+                putPixel((int)Math.Round(x), (int)Math.Round(y), Color.Black);
+
+                if (y2 - y1 >= 0) y += diffy;
+                else y -= diffy;
+
+                if (x2 - x1 >= 0) x += diffx;
+                else x -= diffx;
+
+                i++;
+            }
+        }
+
+        private void MidpointCircle(int x1, int y1, int x2, int y2)
+        {
+            int diffx = (int)Math.Abs(x2 - x1);
+            int diffy = (int)Math.Abs(y2 - y1);
+            int R = (int)Math.Sqrt(diffx * diffx + diffy * diffy);
+
+            int d = 1 - R;
+            int x = 0;
+            int y = R;
+            putPixel(x1 + x, y1 + y, Color.Black);
+            putPixel(x1 + y, y1 + x, Color.Black);
+            putPixel(x1 - y, y1 + x, Color.Black);
+            putPixel(x1 - x, y1 + y, Color.Black);
+            putPixel(x1 - x, y1 - y, Color.Black);
+            putPixel(x1 - y, y1 - x, Color.Black);
+            putPixel(x1 + y, y1 - x, Color.Black);
+            putPixel(x1 + x, y1 - y, Color.Black);
+            while (y > x)
+            {
+                if (d < 0)
+                    d += 2 * x + 3;
+                else
+                {
+                    d += 2 * x - 2 * y + 5;
+                    --y;
+                }
+                ++x;
+                putPixel(x1 + x, y1 + y, Color.Black);
+                putPixel(x1 + y, y1 + x, Color.Black);
+                putPixel(x1 - y, y1 + x, Color.Black);
+                putPixel(x1 - x, y1 + y, Color.Black);
+                putPixel(x1 - x, y1 - y, Color.Black);
+                putPixel(x1 - y, y1 - x, Color.Black);
+                putPixel(x1 + y, y1 - x, Color.Black);
+                putPixel(x1 + x, y1 - y, Color.Black);
+            }
+        }
+
+        private void XLine(int x1, int y1, int x2, int y2)
+        {
+            Color color = Color.Black;
+            Color backcolor = Color.FromArgb(color.R + 175 > 255 ? 255 : color.R + 175,
+                                             color.G + 175 > 255 ? 255 : color.G + 175,
+                                             color.B + 175 > 255 ? 255 : color.B + 175);
+            float diffy = y2 - y1, diffx = x2 - x1;
+            float m = diffy / diffx;
+
+            if (Math.Abs(diffx) > Math.Abs(diffy))
+            {
+                if (x1 > x2)
+                {
+                    int tmp = x1; x1 = x2; x2 = tmp;
+                    tmp = y1; y1 = y2; y2 = tmp;
+                }
+                float y = y1;
+                for (int x = x1; x <= x2; ++x)
+                {
+                    Color c1 = Color.FromArgb((byte)(color.R * (1 - (y - (int)y)) + backcolor.R * (y - (int)y)),
+                                              (byte)(color.G * (1 - (y - (int)y)) + backcolor.G * (y - (int)y)),
+                                              (byte)(color.B * (1 - (y - (int)y)) + backcolor.B * (y - (int)y)));
+                    Color c2 = Color.FromArgb((byte)(color.R * ((y - (int)y)) + backcolor.R * (1 - (y - (int)y))),
+                                              (byte)(color.G * ((y - (int)y)) + backcolor.G * (1 - (y - (int)y))),
+                                              (byte)(color.B * ((y - (int)y)) + backcolor.B * (1 - (y - (int)y))));
+
+                    if (c1.R + c1.G + c1.B > c2.R + c2.G + c2.B)
+                    {
+                        putPixel(x, (int)Math.Floor(y), c1);
+                        putPixel(x, (int)Math.Floor(y) + 1, c2);
+                    }
+                    else
+                    {
+                        putPixel(x, (int)Math.Floor(y) + 1, c2);
+                        putPixel(x, (int)Math.Floor(y), c1);
+                    }
+                    y += m;
+                    pictureBox1.Image = img;
+                }
+            }
+            else
+            {
+                if (y1 > y2)
+                {
+                    int tmp = x1; x1 = x2; x2 = tmp;
+                    tmp = y1; y1 = y2; y2 = tmp;
+                }
+                float x = x1;
+                m = diffx / diffy;
+                for (int y = y1; y < y2; ++y)
+                {
+                    Color c1 = Color.FromArgb((byte)(color.R * (1 - (x - (int)x)) + backcolor.R * (x - (int)x)),
+                                              (byte)(color.G * (1 - (x - (int)x)) + backcolor.G * (x - (int)x)),
+                                              (byte)(color.B * (1 - (x - (int)x)) + backcolor.B * (x - (int)x)));
+                    Color c2 = Color.FromArgb((byte)(color.R * ((x - (int)x)) + backcolor.R * (1 - (x - (int)x))),
+                                              (byte)(color.G * ((x - (int)x)) + backcolor.G * (1 - (x - (int)x))),
+                                              (byte)(color.B * ((x - (int)x)) + backcolor.B * (1 - (x - (int)x))));
+                    if (c1.R + c1.G + c1.B > c2.R + c2.G + c2.B)
+                    {
+                        putPixel((int)Math.Floor(x), y, c1);
+                        putPixel((int)Math.Floor(x) + 1, y, c2);
+                    }
+                    else
+                    {
+                        putPixel((int)Math.Floor(x) + 1, y, c2);
+                        putPixel((int)Math.Floor(x), y, c1);
+                    }
+                    x += m;
+                }
+            }
+        }
+
+        void XCircle(int x1, int y1, int x2, int y2)
+        {
+            Color color = Color.Black;
+
+            Color backcolor = Color.FromArgb(color.R + 100 > 175 ? 175 : color.R + 100,
+                                              color.G + 100 > 175 ? 175 : color.G + 100,
+                                              color.B + 100 > 175 ? 175 : color.B + 100);
+            int dx = (int)Math.Abs(x2 - x1);
+            int dy = (int)Math.Abs(y2 - y1);
+            int R = (int)Math.Sqrt(dx * dx + dy * dy);
+
+            double x = R;
+            int y = 0;
+
+            float T = diam(R, y);
+            Color c1 = Color.FromArgb((byte)(color.R * (1 - T) + backcolor.R * T),
+                                      (byte)(color.G * (1 - T) + backcolor.G * T),
+                                      (byte)(color.B * (1 - T) + backcolor.B * T));
+            Color c2 = Color.FromArgb((byte)(color.R * (T) + backcolor.R * (1 - T)),
+                                      (byte)(color.G * (T) + backcolor.G * (1 - T)),
+                                      (byte)(color.B * (T) + backcolor.B * (1 - T)));
+
+            putPixel(x1 + (int)x, y1 + y, c1);
+            putPixel(x1 - y, y1 + (int)x, c1);
+            putPixel(x1 - (int)x, y1 - y, c1);
+            putPixel(x1 + y, y1 - (int)x, c1);
+
+            for (y = 0; y < (int)x; y++)
+            {
+                T = diam(R, y);
+                c1 = Color.FromArgb((byte)(color.R * (1 - T) + backcolor.R * T),
+                                    (byte)(color.G * (1 - T) + backcolor.G * T),
+                                    (byte)(color.B * (1 - T) + backcolor.B * T));
+                c2 = Color.FromArgb((byte)(color.R * (T) + backcolor.R * (1 - T)),
+                                    (byte)(color.G * (T) + backcolor.G * (1 - T)),
+                                    (byte)(color.B * (T) + backcolor.B * (1 - T)));
+
+                x = Math.Sqrt(R * R - y * y);
+                int f = (int)x % 1;
+                int xleft = (int)(x - f);
+
+                //1st
+                putPixel(x1 + xleft, y1 + y, c1);
+                putPixel(x1 + xleft + 1, y1 + y, c2);
+                //2nd
+                putPixel(x1 + y, y1 + xleft, c1);
+                putPixel(x1 + y, y1 + xleft + 1, c2);
+                //3rd
+                putPixel(x1 - y, y1 + xleft, c1);
+                putPixel(x1 - y, y1 + xleft + 1, c2);
+                //4th
+                putPixel(x1 - xleft, y1 + y, c1);
+                putPixel(x1 - xleft + 1, y1 + y, c2);
+                //5th
+                putPixel(x1 - xleft, y1 - y, c1);
+                putPixel(x1 - xleft + 1, y1 - y, c2);
+                //6th
+                putPixel(x1 - y, y1 - xleft, c1);
+                putPixel(x1 - y, y1 - xleft + 1, c2);
+                //7th
+                putPixel(x1 + y, y1 - xleft, c1);
+                putPixel(x1 + y, y1 - xleft + 1, c2);
+                //8th
+                putPixel(x1 + xleft, y1 - y, c1);
+                putPixel(x1 + xleft + 1, y1 - y, c2);
+            }
+        }
+
+        private float diam(int R, int y)
+        {
+            float x = (float)Math.Sqrt(R * R - y * y);
+            return (float)Math.Ceiling(x) - x;
+        }
+
+        private void PixelCopying(int x1, int y1, int x2, int y2)
+        {
+            //int thick = Int32.Parse(Thick.Text);
+            int thick = 1;
+            if (thick >= 1)
+            {
+                DDA(x1, y1, x2, y2);
+                if (thick >= 3)
+                {
+                    int dx = x2 - x1, dy = y2 - y1;
+                    if (Math.Abs(dx) > Math.Abs(dy))
+                    {
+                        DDA(x1, y1 + 1, x2, y2 + 1);
+                        DDA(x1, y1 - 1, x2, y2 - 1);
+                        if (thick >= 5)
+                        {
+                            DDA(x1, y1 + 2, x2, y2 + 2);
+                            DDA(x1, y1 - 2, x2, y2 - 2);
+                        }
+                    }
+                    else
+                    {
+                        DDA(x1 + 1, y1, x2 + 1, y2);
+                        DDA(x1 - 1, y1, x2 - 1, y2);
+                        if (thick >= 5)
+                        {
+                            DDA(x1 + 2, y1, x2 + 2, y2);
+                            DDA(x1 - 2, y1, x2 - 2, y2);
+                        }
+                    }
+                }
+            }
+        }
+
+        private void putPixel(int x, int y, Color color)
+        {
+            int thick = Int32.Parse(Thick.Text);
+            if (x < 0) x = 0;
+            if (x >= img.Width) x = img.Width - 1;
+            if (y < 0) y = 0;
+            if (y >= img.Height) y = img.Height - 1;
+
+            img.SetPixel(x, img.Height - 1 - y, color);
+            if (thick >= 3)
+            {
+                if (img.Height - 1 - y + 1 < img.Height)
+                    img.SetPixel(x, img.Height - 1 - y + 1, color); // up
+                if (img.Height - 1 - y - 1 >= 0)
+                    img.SetPixel(x, img.Height - 1 - y - 1, color); //down
+                if (x - 1 >= 0)
+                    img.SetPixel(x - 1, img.Height - 1 - y, color);  //left
+                if (x + 1 < img.Width)
+                    img.SetPixel(x + 1, img.Height - 1 - y, color);  //right
+            }
+
+            if (thick >= 5)
+            {
+                if (img.Height - 1 - y + 2 < img.Height)
+                    img.SetPixel(x, img.Height - 1 - y + 2, color); // up2
+                if (img.Height - 1 - y - 2 >= 0)
+                    img.SetPixel(x, img.Height - 1 - y - 2, color); //down2
+                if (img.Height - 1 - y + 1 < img.Height && x - 1 >= 0)
+                    img.SetPixel(x - 1, img.Height - 1 - y + 1, color); // up left
+                if (img.Height - 1 - y + 1 < img.Height && x + 1 < img.Width)
+                    img.SetPixel(x + 1, img.Height - 1 - y + 1, color); // up right
+                if (x - 2 >= 0)
+                    img.SetPixel(x - 2, img.Height - 1 - y, color);  //left2
+                if (x + 2 < img.Width)
+                    img.SetPixel(x + 2, img.Height - 1 - y, color);  //right2
+                if (img.Height - 1 - y - 1 >= 0 && x - 1 >= 0)
+                    img.SetPixel(x - 1, img.Height - 1 - y - 1, color); //down left
+                if (img.Height - 1 - y - 1 >= 0 && x + 1 < img.Width)
+                    img.SetPixel(x + 1, img.Height - 1 - y - 1, color);  //down right
+            }
+        }
+
+        //menu
+        private void dDAToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            dda = true;
+            midpoint = xline = xcircle = copy = false;
+        }
+
+        private void midpointCircleAdditionsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            midpoint = true;
+            dda = xline = xcircle = copy = false;
+        }
+
+        private void lineToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            xline = true;
+            dda = midpoint = xcircle = copy = false;
+        }
+
+        private void circleToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            xcircle = true;
+            dda = midpoint = xline = copy = false;
+        }
+
+        private void thickLineToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            copy = true;
+            dda = midpoint = xline = xcircle = false;
+        }
+
+        private void cleanToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            img = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            pictureBox1.Image = img;
+        }
+    }
+}
